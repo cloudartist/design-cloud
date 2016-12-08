36bc2f69390316d607162c302914467808b46b4c
miszcz <miszcz@miszcz>
2016-12-08 22:17:49 +0000

    Add script to destroy inf

--- bin/clean_inf.sh
+++ bin/clean_inf.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 
+# This script will destroy terraform inf
+
+TERRAFORM_PATH='/repos/aws-home-inf'
+
+function log () {
+    echo "[$(date --rfc-3339=seconds)]: $*" 
+}
+
+log "Trying to destroy infrastructure"
+
+cd $TERRAFORM_PATH && terraform destroy -force -target=module.compute -var-file=environment/local/terraform.tfvars
